74429968d4a8f4d9273502ff7fa4543a40ab1294
android1_hiep <hiepnd230992@gmail.com>
2015-01-21 17:29:35 +0700

    update filr greetingActivity

--- AppNewGroup/src/com/hh/appnewgroup/GreetingsActivity.java
+++ AppNewGroup/src/com/hh/appnewgroup/GreetingsActivity.java
@@ -6,36 +6,60 @@ import com.hh.appnewgroup.adapter.PopularSMSAdapter;
 import com.hh.appnewgroup.db.CategoryObject;
 import com.hh.appnewgroup.db.ReadDB;
 import com.hh.appnewgroup.db.SMSObject;
+import com.manuelpeinado.fadingactionbar.FadingActionBarHelper;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
 public class GreetingsActivity extends Activity {
 
-	private TextView tvBack, tvTitle;
 	private ListView listGreet;
 	private int id_category = 0;
+	private String mStringImg="";
 
 	private ReadDB mReadDB;
-	private ArrayList<CategoryObject> mListTheLoai;
 	private ArrayList<SMSObject> mLisSms;
+	private ImageView image_header;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_greetings);
-		getActionBar().hide();
-		initUI();
+//		setContentView(R.layout.activity_greetings);
+
+		FadingActionBarHelper helper = new FadingActionBarHelper()
+        .actionBarBackground(R.drawable.bg_item_red_actionbar)
+        .headerLayout(R.layout.header)
+        .contentLayout(R.layout.activity_greetings);
+        setContentView(helper.createView(this));
+        helper.initActionBar(this);
+        
+        getActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>Lời chúc"));
+        getActionBar().setIcon(R.drawable.ic_arrow_left);
+        getActionBar().setHomeButtonEnabled(true);
+        ImageView iconImage = (ImageView) findViewById(android.R.id.home);
+        iconImage.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	onBackPressed();
+            }
+        });
+        
+        
+		/*Drawable transparentDrawable = new ColorDrawable(Color.TRANSPARENT);
+		getActionBar().setBackgroundDrawable(transparentDrawable);
+		getActionBar().setTitle("Lời chúc");*/
+		
 		Bundle extras = getIntent().getExtras();
 		id_category = extras.getInt("id_category");
-
+		mStringImg=extras.getString("mStringImg");
 		mReadDB = new ReadDB(GreetingsActivity.this);
 		try {
 			mReadDB.createDatabase();
@@ -43,34 +67,19 @@ public class GreetingsActivity extends Activity {
 		} catch (Exception e) {
 
 		}
-
-		mListTheLoai = new ArrayList<CategoryObject>();
 		mLisSms=new ArrayList<SMSObject>();
-		mListTheLoai = mReadDB.getListTheloai();
 		mLisSms=mReadDB.getlistSMSObject(id_category);
 
 		mReadDB.close();
-		CategoryObject ob = new CategoryObject();
-		for (int i = 0; i < mListTheLoai.size(); i++)
-			if (mListTheLoai.get(i).getId_category() == id_category)
-				ob = mListTheLoai.get(i);
-
-		tvTitle.setText(ob.getName_category());
-		tvBack.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				onBackPressed();
-			}
-		});
+		
+		image_header=(ImageView)findViewById(R.id.image_header);
+		int imgDrawerble = getResources().getIdentifier(mStringImg, "drawable", getPackageName());
+		image_header.setImageResource(imgDrawerble);
+		
+		listGreet = (ListView) findViewById(android.R.id.list);
 		PopularSMSAdapter adapter=new PopularSMSAdapter(GreetingsActivity.this, R.layout.item_list_sms, mLisSms);
 		listGreet.setAdapter(adapter);
 	}
 
-	public void initUI() {
-		tvBack = (TextView) findViewById(R.id.tvBack);
-		tvTitle = (TextView) findViewById(R.id.tvTitle);
-		listGreet = (ListView) findViewById(R.id.listGreet);
-	}
+
 }
